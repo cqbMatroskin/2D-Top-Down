5faa7c1a7bc09fb64e2189db0b2bc6274f7027c5
Alexander Matroskin <cqb.matroskin@gmail.com>
2026-02-19 23:00:40 +0300

    Добавлен выбор карты апгрейда

--- scene/manager/upgrade_manager.gd
+++ scene/manager/upgrade_manager.gd
@@ -18,6 +18,7 @@ func on_lvl_up(current_level: int) -> void:
 	var upgrade_screen_instance = upgrade_screen_scene.instantiate() as UpgradeScreen
 	add_child(upgrade_screen_instance)
 	upgrade_screen_instance.set_ability_upgrades([choosen_upgrade] as Array[AbilityUpgrade])
+	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
 	
 func apply_upgrade(upgrade: AbilityUpgrade) -> void:
 	var has_upgrade: bool = current_upgrades.has(upgrade.id)
@@ -28,3 +29,7 @@ func apply_upgrade(upgrade: AbilityUpgrade) -> void:
 		}
 	else:
 		current_upgrades[upgrade.id]["quantity"] += 1
+	print(current_upgrades)
+
+func on_upgrade_selected(upgrade: AbilityUpgrade) -> void:
+	apply_upgrade(upgrade)
--- scene/ui/upgrade_screen.gd
+++ scene/ui/upgrade_screen.gd
@@ -1,6 +1,8 @@
 extends CanvasLayer
 class_name UpgradeScreen
 
+signal upgrade_selected(upgrade: AbilityUpgrade)
+
 @export var upgrade_card_screen: PackedScene
 
 @onready var card_container: HBoxContainer = $MarginContainer/CardContainer
@@ -13,3 +15,10 @@ func set_ability_upgrades(upgrades: Array[AbilityUpgrade]) -> void:
 		var upgrade_card_instance = upgrade_card_screen.instantiate() as AbilityUpgradeCard
 		card_container.add_child(upgrade_card_instance)
 		upgrade_card_instance.set_ability_upgrade(upgrade)
+		# bind добавляет параметр к эмиту
+		upgrade_card_instance.card_selected.connect(on_upgrade_selected.bind(upgrade))
+
+func on_upgrade_selected(upgrade: AbilityUpgrade) -> void:
+	upgrade_selected.emit(upgrade)
+	get_tree().paused = false
+	queue_free()
